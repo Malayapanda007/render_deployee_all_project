0b933dee21d05fc674183917fdcbf071c85c4895
Malaya07 <pandamalaya55@gmail.com>
2024-12-18 23:30:55 +0530

    adding book recomendation system

--- app.py
+++ app.py
@@ -133,6 +133,67 @@ def predict():
     
     return render_template('email_spam.html', prediction_text='Prediction: {}'.format(result))
 
+
+
+
+import numpy as np
+
+# Load pickled data
+popular_df = pickle.load(open('popular.pkl', 'rb'))
+pt = pickle.load(open('pt.pkl', 'rb'))
+books = pickle.load(open('books.pkl', 'rb'))
+similarity_scores = pickle.load(open('similarity_scores.pkl', 'rb'))
+
+# app = Flask(__name__)
+
+# # Define the home route
+@app.route('/movie_recommendation')
+def movie_recommendation_index():
+    return render_template('index1.html',
+                           book_name=list(popular_df['Book-Title'].values),
+                           author=list(popular_df['Book-Author'].values),
+                           image=list(popular_df['Image-URL-M'].values),
+                           votes=list(popular_df['num-Rating'].values),
+                           rating=list(popular_df['avg-Rating'].values)
+                           )
+
+@app.route('/recommend')
+def recommend_ui():
+    return render_template('recommend.html')
+
+@app.route('/recommend_books', methods=['POST'])
+def recommend():
+    user_input = request.form.get('user_input')
+
+    # Check if the book exists in the pivot table
+    if user_input in pt.index:
+        index = np.where(pt.index == user_input)[0][0]  # Get the index of the user input
+
+        # Get similar items based on the similarity scores
+        similar_items = sorted(list(enumerate(similarity_scores[index])), key=lambda x: x[1], reverse=True)[1:5]
+
+        data = []
+        for i in similar_items:
+            item = []
+            temp_df = books[books['Book-Title'] == pt.index[i[0]]]
+            item.extend(list(temp_df.drop_duplicates('Book-Title')['Book-Title'].values))
+            item.extend(list(temp_df.drop_duplicates('Book-Title')['Book-Author'].values))
+            item.extend(list(temp_df.drop_duplicates('Book-Title')['Image-URL-M'].values))
+
+            data.append(item)
+
+        return render_template('recommend.html', data=data)
+
+    else:
+        # Return an error message if the book name is not found
+        return render_template('index1.html',
+                           book_name=list(popular_df['Book-Title'].values),
+                           author=list(popular_df['Book-Author'].values),
+                           image=list(popular_df['Image-URL-M'].values),
+                           votes=list(popular_df['num-Rating'].values),
+                           rating=list(popular_df['avg-Rating'].values)
+                           )
+
 if __name__ == "__main__":
 
     app.run(debug=True)
